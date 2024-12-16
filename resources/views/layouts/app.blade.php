922c37d50d6cc0ec7e0786f7766a36f64ee1bfe4
nidhi22617 <pnidhi2@hotmail.com>
2024-12-16 16:31:39 +0545

    app.blade title changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>CODE IT NEWS - Admin Dashboard</title>
+    <title>NAGARAIN NEWS - Admin Dashboard</title>
     <!-- General CSS Files -->
     <link rel="stylesheet" href="/assets/css/app.min.css">
     <!-- Template CSS -->
